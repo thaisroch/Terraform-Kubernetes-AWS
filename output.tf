defbc8ceef0c5c714870b7e078bfcc924128ae68
thaisrocha <rochathais.ml@gmail.com>
2024-10-08 21:45:04 -0300

    Add the output file main commented

---
+++ output.tf
@@ -0,0 +1,28 @@
+#output "eks_subnet_private_1a" {
+#  value = module.eks_network.eks_subnet_priv_1a
+#}
+#output "eks_subnet_private_1b" {
+#  value = module.eks_network.eks_subnet_priv_1b
+#}
+#output "eks_subnet_public_1a" {
+#  value = module.eks_network.eks_subnet_pub_1a
+#}
+#output "eks_subnet_public_1b" {
+#  value = module.eks_network.eks_subnet_pub_1b
+#}
+#output "eks_cluster_vpc_config" {
+#  value = module.eks_cluster.eks_vpc_config
+#}
+#output "oidc" {
+#  value = module.eks_cluster.oidc
+#}
+#
+#output "eks_cluste_certificate_authority" {
+#  value = module.eks_cluster.eks_cluste_certificate_authority
+#}
+#output "endpoint" {
+#  value = module.eks_cluster.eks_cluster_endpoint
+#}
+#output "eks_vpc_id" {
+#  value = module.eks_network.eks_vpc_id
+#}
