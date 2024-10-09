4bcd97e51a6e689ca323c2fd3a6bdc093b79fd0c
thaisrocha <rochathais.ml@gmail.com>
2024-10-08 21:41:54 -0300

    Add the file providers

--- provider.tf
+++ provider.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.67.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.15.0"
+    }
   }
   backend "s3" {
     bucket = "projeto-devops-bucket"
@@ -18,3 +26,26 @@ provider "aws" {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+
+# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
+provider "kubernetes" {
+  host                   = module.eks_cluster.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.eks_cluste_certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.eks_cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.eks_cluste_certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.eks_cluster_name]
+      command     = "aws"
+    }
+  }
+}
