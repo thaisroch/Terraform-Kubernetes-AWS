c7010367932ad3df66c9c2cf5fccfd8962c0fc9c
thaisrocha <rochathais.ml@gmail.com>
2024-10-08 21:41:05 -0300

    Feat: Added the module eks_mng and aws_load_balancer_controller

--- modules.tf
+++ modules.tf
@@ -4,11 +4,26 @@ module "eks_network" {
   project_name = var.project_name
   tags         = local.tags
 }
-
 module "eks_cluster" {
   source           = "./modules/cluster"
   project_name     = var.project_name
   tags             = local.tags
   public_subnet_1a = module.eks_network.eks_subnet_pub_1a
   public_subnet_1b = module.eks_network.eks_subnet_pub_1b
+}
+module "eks_mng" {
+  source            = "./modules/managed-node-group"
+  project_name      = var.project_name
+  tags              = local.tags
+  eks_cluster_name  = module.eks_cluster.eks_cluster_name
+  private_subnet_1a = module.eks_network.eks_subnet_priv_1a
+  private_subnet_1b = module.eks_network.eks_subnet_priv_1b
+}
+module "eks_aws_load_balancer_controller" {
+  source           = "./modules/aws-load-balancer-controller"
+  project_name     = var.project_name
+  tags             = local.tags
+  oidc             = module.eks_cluster.oidc
+  eks_cluster_name = module.eks_cluster.eks_cluster_name
+  eks_vpc_id       = module.eks_network.eks_vpc_id
 }
